baa4d78674f91685c67da2fae462e5e75e4a90db
Eran <110078@st-maartenscollege.nl>
2020-09-11 08:45:41 +0200

    Added PersonModel.cs

---
+++ mvc-template-master/SchoolTemplate/Models/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SchoolTemplate.Models
+{
+    public class PersonModel
+    {
+        public string firstname { get; set; }
+
+        public string lastname { get; set; }
+
+        public DateTime birthday { get; set; }
+    }
+}
