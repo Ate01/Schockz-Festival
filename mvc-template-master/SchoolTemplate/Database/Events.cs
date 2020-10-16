91646527d78eb90b3d05a1db50f75cbe8f85456d
eran78 <110078@st-maartenscollege.nl>
2020-10-16 17:00:39 +0200

    Fixed code comment at events.cs and added code comment to PersonModel.cs, row 10

--- mvc-template-master/SchoolTemplate/Database/Events.cs
+++ mvc-template-master/SchoolTemplate/Database/Events.cs
@@ -2,7 +2,7 @@
 
 namespace SchoolTemplate.Database
 {
-    public class Event //this public class links to the public class event
+    public class Event //this public class links to the public class event in the home controller
     {
         public int id { get; set; } //primary key of database, always shown as an integer
 
